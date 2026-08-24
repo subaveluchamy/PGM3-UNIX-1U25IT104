2983f9b3787172fd1724757ead816fc73125ba44
subaveluchamy <subaveluchamy0608@gmail.com>
2026-08-24 21:58:06 +0530

    PGM3 UNIX 1U25IT104
    Removed echo statements and unnecessary comments.

--- mkdir_practical.sh
+++ mkdir_practical.sh
@@ -1,17 +1,7 @@
-#!/bin/bash
-
-echo "Creating a single directory..."
-
 mkdir bscit
-
-echo "Creating parent directories..."
 
 mkdir -p /home/user/my_folder/sub_folder
 
-echo "Creating multiple directories..."
-
 mkdir projects documents downloads
-
-echo "Listing directories..."
 
 ls
